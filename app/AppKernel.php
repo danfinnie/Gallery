e76f681e552526a75ca7da3f21f2b094467d87a2
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-22 13:26:49 -0500

    Install Sonata default configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
