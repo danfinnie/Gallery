3584ed139f18cd735fbecd31f5432f5214d0746d
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-23 14:08:26 -0500

    Move parameters to untracked file location, change DB passwords

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
 php composer.phar install
+php composer.phar dumpautoload # Only some environments need this...
 php app/console doctrine:generate:entities DanFinnie --path src/
+cp app/config/parameters.yml{dist,}
 php app/console assets:install web
 php app/console cache:clear
+
+echo "Please modify app/config/parameters.yml with correct values"
