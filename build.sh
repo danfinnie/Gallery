b31f32bf0beea485ff51b51705d912de83e9ec1c
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-24 14:56:56 -0500

    Add SonataMediaBundle generation to build script

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 php composer.phar install
 php composer.phar dumpautoload # Only some environments need this...
 php app/console doctrine:generate:entities DanFinnie --path src/
-php app/console sonata:easy-extends:generate SonataMediaBundle
-php app/console doctrine:schema:create
-cp app/config/parameters.yml{dist,}
+php app/console sonata:easy-extends:generate SonataMediaBundle --dest=src/ 
+cp app/config/parameters.yml{.dist,}
 
 echo "Please modify app/config/parameters.yml with correct values.  Then press enter to continue."
 read
 
+php app/console doctrine:schema:create
 php app/console assets:install web
 php app/console cache:clear
 
