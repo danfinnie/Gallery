70a8a20ac9dfbcf1de0b1982d59e09e00a97c7e2
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-26 16:02:36 -0500

    Basic showCategories action

--- src/DanFinnie/GalleryBundle/Controller/GalleryController.php
+++ src/DanFinnie/GalleryBundle/Controller/GalleryController.php
@@ -8,7 +8,10 @@ class GalleryController extends Controller
 {
     public function showCategoriesAction()
     {
-        return $this->render('DanFinnieGalleryBundle:Default:index.html.twig', array('name' => "yes"));
+    	$CategoriesRepo = $this->get('doctrine')->getRepository('DanFinnieGalleryBundle:Category');
+    	$categories = $CategoriesRepo->findAll(array(), array("title"));
+
+        return $this->render('DanFinnieGalleryBundle:Gallery:category_list.html.twig', array('categories' => $categories));
     }
 
     public function showCategoryAction($id)
