7793675c01d9ae911e9ec0a9502e907851816dc7
Dan Finnie <dfinnie@noisenewyork.com>
2013-03-03 17:26:59 -0500

    Refactor GalleryController to use the same grid for category and home page display

--- src/DanFinnie/GalleryBundle/Controller/GalleryController.php
+++ src/DanFinnie/GalleryBundle/Controller/GalleryController.php
@@ -8,23 +8,38 @@ class GalleryController extends Controller
 {
     public function showCategoriesAction()
     {
+        $Router = $this->get('router');
     	$CategoriesRepo = $this->get('doctrine')->getRepository('DanFinnieGalleryBundle:Category');
     	$categories = $CategoriesRepo->findAll(array(), array("title"));
 
-        return $this->render('DanFinnieGalleryBundle:Gallery:category_list.html.twig', array('categories' => $categories));
+        return $this->render('DanFinnieGalleryBundle:Gallery:grid_display.html.twig', array(
+            'title' => "Zackerman's Gallery",
+            'grid' => array_map(function($Category) use ($Router) {
+                return array(
+                    "title" => $Category->getTitle(),
+                    "path" => $Router->generate('dan_finnie_gallery_category_page', array('id' => $Category->getId())),
+                );
+            }, $categories),
+        ));
     }
 
     public function showCategoryAction($id)
     {
+        $Router = $this->get('router');
         $CategoriesRepo = $this->get('doctrine')->getRepository('DanFinnieGalleryBundle:Category');
         $PiecesRepo = $this->get('doctrine')->getRepository('DanFinnieGalleryBundle:Piece');
 
         $Category = $CategoriesRepo->find($id);
         $pieces = $PiecesRepo->findAll(array("category" => $Category), array("order"));
 
-        return $this->render('DanFinnieGalleryBundle:Gallery:category.html.twig', array(
-            'pieces' => $pieces,
-            'category' => $Category
+        return $this->render('DanFinnieGalleryBundle:Gallery:grid_display.html.twig', array(
+            'title' => $Category->getTitle(),
+            'grid' => array_map(function($Piece) use ($Router) {
+                return array(
+                    "title" => $Piece->getTitle(),
+                    "path" => $Router->generate('dan_finnie_gallery_piece_page', array('id' => $Piece->getId())),
+                );
+            }, $pieces),
         ));
     }
 
