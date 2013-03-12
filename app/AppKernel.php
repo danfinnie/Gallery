df90475be90ecdcc8441ea03bc692e76944b97b2
Dan Finnie <dan@danfinnie.com>
2013-03-12 15:37:56 -0400

    Install Doctrine migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
             new DanFinnie\SiteBundle\DanFinnieSiteBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
